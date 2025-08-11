f9a89e58473d6055b6e741cb2be5a182941454a6
Atharva708 <112915510+Atharva708@users.noreply.github.com>
2025-08-11 12:24:50 +0530

    push 0.0.2

---
+++ DEFENDO-AI/Services/SupabaseClient.swift
@@ -0,0 +1,10 @@
+//
+//  SupabaseClient.swift
+//  DEFENDO-AI
+//
+//  Created by Atharva Gour on 8/11/25.
+//
+
+import Supabase
+
+let client = SupabaseClient(supabaseURL: URL(string: "https://xyzcompany.supabase.co")!, supabaseKey: "public-anon-key")
