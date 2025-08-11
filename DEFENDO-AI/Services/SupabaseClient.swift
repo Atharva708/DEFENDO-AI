d85bf17c8068106b59fd9cba5e6f927ef72d8146
Atharva708 <112915510+Atharva708@users.noreply.github.com>
2025-08-11 12:34:44 +0530

    Added supabase credentials 0.0.2

--- DEFENDO-AI/Services/SupabaseClient.swift
+++ DEFENDO-AI/Services/SupabaseClient.swift
@@ -7,4 +7,4 @@
 
 import Supabase
 
-let client = SupabaseClient(supabaseURL: URL(string: "https://xyzcompany.supabase.co")!, supabaseKey: "public-anon-key")
+let client = SupabaseClient(supabaseURL: URL(string: "https://lslcgkwhdouspopvbkup.supabase.co")!, supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxzbGNna3doZG91c3BvcHZia3VwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NDg5NDEwNSwiZXhwIjoyMDcwNDcwMTA1fQ.sNuUt8eOg-43lAYFY9tOIgWfo0j8Yqe2AHuP6UvqUjI")
